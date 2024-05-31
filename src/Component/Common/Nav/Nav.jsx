80a9c6e86e5e8cf4c3e853c3053d36a1f9121ae0
Husnain Tanveer <98305154+husnain900@users.noreply.github.com>
2024-05-31 16:29:48 +0500

    update

---
+++ src/Component/Common/Nav/Nav.jsx
@@ -0,0 +1,21 @@
+
+const Nav = () => {
+    return (
+        <div>
+            <Nav>
+                <div className="container">
+                    <div className="d-flex">
+                        <div className="logo">
+                            <img src='Assets/Images/Logo/fathena.png' alt="logo" />
+                        </div>
+                        <div className="menu">
+                            <img src='Assets/Images/mennu.png' alt="logo" />
+                        </div>
+                    </div>
+                </div>
+            </Nav>
+        </div>
+    )
+}
+
+export default Nav
