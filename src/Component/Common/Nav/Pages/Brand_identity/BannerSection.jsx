aee1aa0e59a253f33bbe05ec3b757094c86a4412
MianAafi <aftabfarooq8@gmail.com>
2024-05-31 20:20:12 +0500

    fggg

---
+++ src/Component/Common/Nav/Pages/Brand_identity/BannerSection.jsx
@@ -0,0 +1,31 @@
+
+import './BrandIdentity.css';
+import { IoCheckmarkCircle } from "react-icons/io5";
+
+
+
+function BannerSection() {
+  return (
+    <div className='Brand_Identity_bg'>
+    <div className="container">
+    <div className="row ">
+        <div className="col-6 ">
+            <h2 className='Brand_Heading'>BRAND</h2>
+            <h2 className='Identity_Heading'>Identity</h2>
+            <div className='BrandIdentity_P'>
+                <p><IoCheckmarkCircle  /> {" "}
+Stand out from the crowd</p>
+<p><IoCheckmarkCircle  /> {" "}
+Communicate company’s personality</p>
+<p><IoCheckmarkCircle  /> {" "}
+Comprehensive brand strategy planning</p>
+            </div>
+        </div>
+        <div className="col-6"></div>
+    </div>
+
+</div></div>
+  )
+}
+
+export default BannerSection
--- src/Component/Common/Nav/Pages/Brand_identity/BrandIdentity.jsx
+++ src/Component/Common/Nav/Pages/Brand_identity/BrandIdentity.jsx
@@ -1,17 +1,13 @@
 
+import BannerSection from './BannerSection';
 import './BrandIdentity.css';
+
 function BrandIdentity() {
   return (
-    <div className="container_fluid">
-        <div className="row Brand_Identity_bg">
-            <div className="col-6">
-                <h2>BRAND</h2>
-                <h2><b>Identity</b></h2>
-            </div>
-            <div className="col-6"></div>
-        </div>
-
-    </div>
+  <>
+  <BannerSection/>
+  
+  </>
   )
 }
 
