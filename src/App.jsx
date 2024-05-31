2a70bb7df07b5b0cb6d65485a3ca004e5aaae429
Husnain Tanveer <98305154+husnain900@users.noreply.github.com>
2024-05-31 16:34:27 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Nav from './Component/Common/Nav/Nav';
 
 // Define some simple components for the pages
 const Home = () => <h2>Home Page</h2>;
-const About = () => <h2>About Page</h2>;
+// const About = () => <h2>About Page</h2>;
 const Contact = () => <h2>Contact Page</h2>;
 
 function App() {
   return (
     <Router>
- 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
- 
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<Nav />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
     </Router>
   );
 }
