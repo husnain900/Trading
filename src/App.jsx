b145a37ae73087e56500adaafa104609445397d3
MianAafi <aftabfarooq8@gmail.com>
2024-06-01 13:07:36 +0500

    cxczx

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Nav from './Component/Common/Nav/Nav';
 import Footer from './Component/Common/Footer/Footer';
+import BrandIdentity from './Component/Common/Nav/Pages/Brand_identity/BrandIdentity';
 
 
-
-// Define some simple components for the pages
-const Home = () => <h2>Home Page</h2>;
 
 function App() {
   return (
     <Router>
       <Nav />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<BrandIdentity />} />
       </Routes>
       <Footer />
     </Router>
