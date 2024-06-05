75fd590a594decf42d4f5ca527d149336946f35b
Husnain Tanveer <98305154+husnain900@users.noreply.github.com>
2024-06-05 20:05:59 +0500

    update

--- src/Component/Pages/AboutUs/AboutUsSection.jsx
+++ src/Component/Pages/AboutUs/AboutUsSection.jsx
@@ -1,3 +1,4 @@
+import './About.css';
 
 const carouselData = [
   {
@@ -8,20 +9,37 @@ const carouselData = [
   },
   {
     src: "/images/carosal.png",
-    label: "Innovation at Every Step ",
-    content: "Creativity and Problem-Solving: Innovation is in our DNA. We encourage creative thinking and problem-solving to overcome challenges and deliver cutting-edge solutions. Continuous Improvement: We believe there’s always room for growth. Athena fosters a culture of continuous improvement, where learning and adapting are part of our everyday work.",
+    label: "Innovation at Every Step",
+    content: "<strong>Creativity and Problem-Solving:</strong> Innovation is in our DNA. We encourage creative thinking and problem-solving to overcome challenges and deliver cutting-edge solutions.",
+    content2: "<strong>Continuous Improvement:</strong> We believe there’s always room for growth. Athena fosters a culture of continuous improvement, where learning and adapting are part of our everyday work.",
     interval: 2000
   },
   {
     src: "/images/carosal.png",
-    label: "Integrity and Transparency ",
-    content: "Ethical Standards: Integrity is at the core of all we do. We uphold the highest standards of professionalism and ethics in our work and interactions. Open Communication: Transparent communication forms the basis of our operations. We believe in open dialogues, where every voice is heard and valued.",
+    label: "Integrity and Transparency",
+    content: "<strong>Ethical Standards:</strong> Integrity is at the core of all we do. We uphold the highest standards of professionalism and ethics in our work and interactions.",
+    content2: "<strong>Open Communication:</strong> Transparent communication forms the basis of our operations. We believe in open dialogues, where every voice is heard and valued.",
+    interval: 1000 // Default interval
+  },
+  {
+    src: "/images/carosal.png",
+    label: "Diversity and Inclusion",
+    content: "<strong>A Melting Pot of Talents:</strong> Athena is proud of its diverse workforce. We bring together people from various backgrounds, each adding unique value to our collective goals.",
+    content2: "<strong>Inclusivity:</strong> We are committed to creating an inclusive environment where everyone feels respected, regardless of their background or identity.",
+    interval: 500 // Default interval
+  },
+  {
+    src: "/images/carosal.png",
+    label: "Empowering Our People",
+    content: "<strong>Professional Growth:</strong>  At Athena, career development is a priority. We provide opportunities for learning, mentorship, and advancement.",
+    content2: "<strong>Recognition and Reward:</strong>We recognize and reward the hard work and achievements of our team members, celebrating individual and collective successes.",
     interval: null // Default interval
   },
   {
     src: "/images/carosal.png",
-    label: "Third slide label",
-    content: "Some representative placeholder content for the third slide.",
+    label: "Social Responsibility",
+    content: "<strong>Community Engagement:</strong>Athena is an active participant in community development. We encourage our employees to engage in volunteer work and support various local initiatives.",
+    content2: "<strong>Environmental Stewardship:</strong>We are mindful of our environmental impact and strive to operate sustainably, contributing positively",
     interval: null // Default interval
   }
 ];
@@ -59,9 +77,10 @@ function AboutUsSection() {
                   <img src={item.src} className="d-block" alt={`${item.label}`} />
                 </div>
                 <div className="col-12 col-md-6">
-                  <div className="d-none d-md-block">
-                    <h5>{item.label}</h5>
-                    <p>{item.content}</p>
+                  <div className="d-md-block mt-5 mt-lg-0">
+                    <h5 className='fw-bold'>{item.label}</h5>
+                    <p dangerouslySetInnerHTML={{ __html: item.content }}></p>
+                    {item.content2 && <p dangerouslySetInnerHTML={{ __html: item.content2 }}></p>}
                   </div>
                 </div>
               </div>
