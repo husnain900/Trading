fea0960f987b4076cc5fe220ef99fd301ca224d4
MianAafi <aftabfarooq8@gmail.com>
2024-06-13 15:21:48 +0500

    fbf

--- src/Component/Pages/WebDesign/WebDesignSection4.jsx
+++ src/Component/Pages/WebDesign/WebDesignSection4.jsx
@@ -3,8 +3,8 @@ import "./WebDesignSection4.css";
 function WebDesignSection4() {
   return (
     <div className="container">
-    <div className="row d-flex justify-content-around  " style={{marginTop:'90px'}} >
-<div className="   col-xl-7 col-md-9 col-12  BRandIdentityBrandStrategy">
+    <div className="row d-flex justify-content-around  " >
+<div className="   col-xl-7 col-md-9 col-12  BRandIdentityBrandStrategy  WebdesignS4Mobile">
         <h2>Mobile Responsive Design</h2>
 <h3> A mobile responsive design of your brand allows customers the ability to access your business – anytime, anywhere.</h3>
     </div>
