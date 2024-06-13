a18e02a12f9576b11486ffae125f61ee8845eecb
Husnain Tanveer <98305154+husnain900@users.noreply.github.com>
2024-06-14 00:34:53 +0500

    update

--- src/Component/Pages/WebMaintaince/WebmaintainceSection4.jsx
+++ src/Component/Pages/WebMaintaince/WebmaintainceSection4.jsx
@@ -2,22 +2,28 @@
 function WebmaintainceSection4() {
   return (
     <div className="container">
-    <div className="row d-flex justify-content-around " >
-<div className="   col-xl-8 col-md-10 col-12  BRandIdentityBrandStrategy">
-        <h2>Web Management</h2>
-<h3>Ensuring your website stays ahead of the game.</h3>
-    </div>
-    
-    
-    </div>
-
-    <div className="row" >
-        <div className="col-12 BrandIdentitys3_image">
-            <img src="/images/maintaince2.png" alt="images"></img>
+      <div className="row d-flex justify-content-around " >
+        <div className="   col-xl-8 col-md-10 col-12  BRandIdentityBrandStrategy">
+          <h2>Web Management</h2>
+          <h3>Ensuring your website stays ahead of the game.</h3>
         </div>
-    </div>
 
-</div>
+
+      </div>
+
+      <div className="row" >
+        <div className="col-12 BrandIdentitys3_image">
+          <img src="/images/maintaince2.png" alt="images"></img>
+          <div className="d-block d-lg-none">
+            <p className="section-text2 text-black pb-3">Security and features audits.</p>
+            <p className="section-text2 text-black pb-3">Module and theme updates.</p>
+            <p className="section-text2 text-black pb-3">Integrate new functionalities.</p>
+            <p className="section-text2 text-black pb-3">Platform, plug-in and software updates.</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
