d5b51c41000a8d7b544566b80891d2524e7a64a7
Husnain Tanveer <98305154+husnain900@users.noreply.github.com>
2024-06-14 02:35:04 +0500

    kugi

--- src/Component/Pages/Brand_identity/BrandIdentitySection2.jsx
+++ src/Component/Pages/Brand_identity/BrandIdentitySection2.jsx
@@ -3,29 +3,26 @@ import "./BrandIdentitySection2.css";
 function BrandIdentitySection2() {
   return (
     <div className="container">
-        <div className="row d-flex justify-content-around " >
-<div className="   col-xl-6 col-md-7 col-12  BRandIdentityBrandStrategy">
-            <h2>Brand Strategy 
-& Visual Identity</h2>
-<h3>With the massive amount of brands in today’s markets, its essential to distinguish your brand from others through developing a brand strategy.</h3>
+      <div className="row d-flex justify-content-around " >
+        <div className="   col-xl-6 col-md-7 col-12  BRandIdentityBrandStrategy">
+          <h2>Brand Strategy
+            & Visual Identity</h2>
+          <h3>With the massive amount of brands in today’s markets, its essential to distinguish your brand from others through developing a brand strategy.</h3>
         </div>
-        
-        
+      </div>
+      <div className="row" >
+        <div className="col-12 BrandIdentitys3_image ">
+          <img className="BrandImagedisplayno" src="/images/brandidentitys3.png" alt="images"></img>
+          <div className="BrandImagedisplaysmallscreen">
+            <img src="/images/Brand1.png" alt="images"></img>
+            <img src="/images/Brand2.png" alt="images"></img>
+            <img src="/images/Brand3.png" alt="images"></img>
+            <img src="/images/Brand4.png" alt="images"></img>
+            <img src="/images/Brand5.png" alt="images"></img>
+            <img src="/images/Brand6.png" alt="images"></img>
+          </div>
         </div>
-
-        <div className="row" >
-            <div className="col-12 BrandIdentitys3_image ">
-                <img className="BrandImagedisplayno" src="/images/brandidentitys3.png" alt="images"></img>
-                <div className="BrandImagedisplaysmallscreen">
-                <img  src="/images/Brand1.png" alt="images"></img>
-                <img  src="/images/Brand2.png" alt="images"></img>
-                <img  src="/images/Brand3.png" alt="images"></img>
-                <img  src="/images/Brand4.png" alt="images"></img>
-                <img  src="/images/Brand5.png" alt="images"></img>
-                <img  src="/images/Brand6.png" alt="images"></img>
-                </div>
-            </div>
-        </div>
+      </div>
 
     </div>
   )
